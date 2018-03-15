f4c7944727b8f8b5d60e73e1978779e74185e184
kot903491 <kot903491@gmail.com>
2018-03-15 17:06:20 +0500

    ДЗ 7

--- controllers/NoteController.php
+++ controllers/NoteController.php
@@ -21,7 +21,7 @@ class NoteController extends Controller
     {
         return [
             'verbs' => [
-                'class' => VerbFilter::class,
+                'class' => VerbFilter::className(),
                 'actions' => [
                     'delete' => ['POST'],
                 ],
